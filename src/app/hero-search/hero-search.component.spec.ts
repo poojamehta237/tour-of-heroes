bd826d7ef65fec4db3c5918d76fab78ecaef2e45
Pooja <poojamehta237@gmail.com>
2019-02-27 19:04:45 +0530

    [Second Commit]

--- src/app/hero-search/hero-search.component.spec.ts
+++ src/app/hero-search/hero-search.component.spec.ts
@@ -1,19 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { HeroDetailComponent } from './hero-detail.component';
 
-describe('HeroDetailComponent', () => {
-  let component: HeroDetailComponent;
-  let fixture: ComponentFixture<HeroDetailComponent>;
+import { HeroSearchComponent } from './hero-search.component';
+
+describe('HeroSearchComponent', () => {
+  let component: HeroSearchComponent;
+  let fixture: ComponentFixture<HeroSearchComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeroDetailComponent ]
+      declarations: [ HeroSearchComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HeroDetailComponent);
+    fixture = TestBed.createComponent(HeroSearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
-import { HeroService} from '../hero.service';
+import { HeroService } from '../hero.service';
 
 @Component({   //@Component is decorator that specifies angular metadata for component
   selector: 'app-heroes',       //component's css element selector
@@ -9,25 +9,31 @@ import { HeroService} from '../hero.service';
   styleUrls: ['./heroes.component.css']   //location of component's private css file
 })
 export class HeroesComponent implements OnInit {
-  
-  //heroes = HEROES;  //since not importing HEROES from mock-heroes
-  heroes : Hero[];       //so we use it from hero.service
-  // selectedHero : Hero;
-  
-  //Add a private heroService parameter of type HeroService to the constructor.
+  heroes: Hero[];
+
   constructor(private heroService: HeroService) { }
 
-  //Angular calls ngOnInit shortly after creating a component
   ngOnInit() {
     this.getHeroes();
   }
-  // onSelect(hero:Hero):void{
-  //   this.selectedHero = hero;
-  // }
-  getHeroes(): void{
+
+  getHeroes(): void {
     this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes);
-  } 
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
 }
-
-
